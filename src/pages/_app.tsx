5e67839dbe6685adf4290715122365946007c35c
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2022-08-20 23:02:01 +0700

    build: prepare frontend mentor project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css';
+import { Fragment } from 'react';
+
 import type { AppProps } from 'next/app';
 
+import Head from 'next/head';
+import '../styles/globals.css';
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 
 export default MyApp;
