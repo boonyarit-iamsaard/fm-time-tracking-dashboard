8e4c00c9f7f4fedb0ec24b454b5a34e9e7606385
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2022-08-25 15:19:56 +0700

    build: prepare frontend mentor project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,65 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+  <title>Frontend Mentor | Time tracking dashboard</title>
+</svelte:head>
+
+Report for Jeremy Robson Daily Weekly Monthly Work 5hrs <!-- daily -->
+Previous - 7hrs <!-- daily -->
+32hrs <!-- weekly -->
+Previous - 36hrs <!-- weekly -->
+103hrs <!-- monthly -->
+Previous - 128hrs <!-- monthly -->
+
+Play 1hr <!-- daily -->
+Previous - 2hrs <!-- daily -->
+10hrs <!-- weekly -->
+Previous - 8hrs <!-- weekly -->
+23hrs <!-- monthly -->
+Previous - 29hrs <!-- monthly -->
+
+Study 0hrs <!-- daily -->
+Previous - 1hr <!-- daily -->
+4hrs <!-- weekly -->
+Previous - 7hrs <!-- weekly -->
+13hrs <!-- monthly -->
+Previous - 19hrs <!-- monthly -->
+
+Exercise 1hr <!-- daily -->
+Previous - 1hr <!-- daily -->
+4hrs <!-- weekly -->
+Previous - 5hrs <!-- weekly -->
+11hrs <!-- monthly -->
+Previous - 18hrs <!-- monthly -->
+
+Social 1hr <!-- daily -->
+Previous - 3hrs <!-- daily -->
+5hrs <!-- weekly -->
+Previous - 10hrs <!-- weekly -->
+21hrs <!-- monthly -->
+Previous - 23hrs <!-- monthly -->
+
+Self Care 0hrs <!-- daily -->
+Previous - 1hr <!-- daily -->
+2hrs <!-- weekly -->
+Previous - 2hrs <!-- weekly -->
+7hrs <!-- monthly -->
+Previous - 11hrs <!-- monthly -->
+
+<div class="attribution">
+  Challenge by <a
+    href="https://www.frontendmentor.io/challenges/time-tracking-dashboard-UIQ7167Jw"
+    target="_blank">Frontend Mentor | Time tracking dashboard</a
+  >. Coded by
+  <a href="https://github.com/boonyarit-iamsaard" target="_blank"
+    >Boonyarit Iamsa-ard</a
+  >.
+</div>
+
+<style lang="css">
+  .attribution {
+    font-size: 0.75rem;
+    text-align: center;
+  }
+  .attribution a {
+    color: hsl(228, 45%, 44%);
+  }
+</style>
